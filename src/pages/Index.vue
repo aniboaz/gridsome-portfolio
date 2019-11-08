931cca8c77db463640973410e3e6e9ead9966a99
aniboaz <aniboaz@gmail.com>
2019-11-08 11:56:43 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,15 +10,7 @@
       </div>
     </div> <!-- end hero -->
 
-
-    <div class="container mx-auto px-4">
-
-          <section class="py-12 px-4 text-center">
-            <div class="w-full max-w-2xl mx-auto"><span class="text-sm font-semibold">TAGLINE</span>
-              <h2 class="text-5xl mt-2 mb-6 leading-tight font-heading">No paper plane can be made without paper</h2><a class="text-blue-700 hover:underline" href="#">Learn more &raquo;</a>
-            </div>
-          </section>
-
+<div class="container mx-auto px-4">
         <Advantages />
 </div>
           <section class="bg-green-300 py-12 px-4"><div class="flex flex-wrap -mx-4">
