c01aedcc8db50577d4ee29e3983aef1dfaa80975
aniboaz <aniboaz@gmail.com>
2019-11-08 08:54:28 +0200

    blog in homepage test

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -38,28 +38,16 @@
 
           <section class="py-12 px-4">
             <h2 class="text-3xl text-center mb-8 font-heading">Latest posts</h2>
+
             <div class="flex flex-wrap -mx-4">
-              <div class="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
+            <div v-for="post in $page.posts.edges" :key="post.id" class="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
                 <div class="h-full pb-8 rounded shadow-md"><a href="#"><img class="mb-4" src="placeholders/pictures/work.jpg" alt="">
                     <div class="px-6"><small>22 Oct 2019 | By Michael Scott</small>
-                      <h3 class="text-xl my-3 font-heading">Being world’s best boss</h3>
-                      <p class="text-gray-500">The wise man once said: "Friend first, boss second, entertainer third". That man was me.</p>
+                      <h3 class="text-xl my-3 font-heading"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h3>
+                      <p class="text-gray-500">  {{ post.node.summary }}</p>
                     </div></a></div>
               </div>
-              <div class="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
-                <div class="h-full pb-8 rounded shadow-md"><a href="#"><img class="mb-4" src="placeholders/pictures/office.jpg" alt="">
-                    <div class="px-6"><small>22 Oct 2019 | By Dwight K. Schrute</small>
-                      <h3 class="text-xl my-3 font-heading">Understanding the paper</h3>
-                      <p class="text-gray-500">So you think you know everything about paper? False, you know nothing. Let me illuminate your first steps into my world.</p>
-                    </div></a></div>
-              </div>
-              <div class="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
-                <div class="h-full pb-8 rounded shadow-md"><a href="#"><img class="mb-4" src="placeholders/pictures/work.jpg" alt="">
-                    <div class="px-6"><small>22 Oct 2019 | By Jim Halpert</small>
-                      <h3 class="text-xl my-3 font-heading">Dwight doesn’t understand the paper</h3>
-                      <p class="text-gray-500">Read it and finally find out that Dwight Schrute is merely a scammer.</p>
-                    </div></a></div>
-              </div>
+          
             </div>
           </section>
 
