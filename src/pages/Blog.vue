24c411c4aecaad2d925bf949981137a0d83329dd
aniboaz <aniboaz@gmail.com>
2020-11-09 01:14:41 +0200

    Update Blog.vue

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -75,7 +75,7 @@
 
 <page-query>
 query Posts ($page: Int) {
-  posts: allPost (sortBy: "date", order: DESC, perPage: 3, page: $page) @paginate {
+  posts: allPost (sortBy: "date", order: DESC, perPage: 4, page: $page) @paginate {
     totalCount
     pageInfo {
       totalPages
