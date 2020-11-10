9be0ec9dd493f3e392d89224fb6b19c22525f4a4
aniboaz <aniboaz@gmail.com>
2020-11-10 10:04:40 +0200

    Update Post.vue

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,20 +1,20 @@
 <template>
   <Layout><VueScrollProgress></VueScrollProgress>
     <div class="container-inner mx-auto my-16">
-      <h1 class="text-4xl font-bold leading-tight">{{ $page.post.title }}</h1>
-      <div class="text-xl text-gray-600 mb-4">{{ $page.post.date }}</div>
+      <h1 class="text-6xl font-serif font-bold leading-tight">{{ $page.post.title }}</h1>
+      <div class="text-xl text-copy-secondary mb-4">{{ $page.post.date }}</div>
       <div class="flex mb-8 text-sm">
         <g-link
           :to="tag.path"
           v-for="tag in $page.post.tags"
           :key="tag.id"
-          class="bg-gray-300 rounded-full px-4 py-2 mr-4 hover:bg-green-300">
+          class="bg-background-tertiary rounded-full px-4 py-2 mr-4 hover:bg-background-secondary">
           {{ tag.title }}
         </g-link>
       </div>
       <div class="markdown-body mb-8" v-html="$page.post.content" />
       <div class="mb-8">
-        <g-link to="/blog" class="font-bold uppercase">Back to Blog</g-link>
+        <g-link to="/blog" class="font-bold">Back to Blog</g-link>
       </div>
     </div>
   </Layout>
